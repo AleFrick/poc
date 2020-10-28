b9ec111a11f7e1a177003c67b65d8b98471f305f
Alex Schmidt <alex.schmidt@avmb.com.br>
2020-10-28 15:25:26 -0300

    'teste'

---
+++ backend2/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3003
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}..`)
+});
+
+
+app.get('/', (req, res) => {
+    res.send(`teste`);
+});
